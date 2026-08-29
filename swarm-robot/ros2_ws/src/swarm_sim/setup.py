7dcc1d8ed797283a5c71a384f34b6a3327bedc53
poojan12345 <59503812+poojan12345@users.noreply.github.com>
2026-08-29 14:39:55 +0530

    Add Python package setup for swarm simulation

---
+++ swarm-robot/ros2_ws/src/swarm_sim/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from glob import glob
+import os
+
+package_name = 'swarm_sim'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.sdf')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    entry_points={'console_scripts': ['swarm_coordinator = swarm_sim.swarm_coordinator:main']},
+)
